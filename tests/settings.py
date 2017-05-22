5462ee1880924a9d8d108e68c437559470f80ec1
Alexander Schepanovski <suor.web@gmail.com>
2017-05-22 13:05:54 +0700

    Set up sqlite db

--- tests/settings.py
+++ tests/settings.py
@@ -6,8 +6,8 @@ INSTALLED_APPS = [
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': 'pickling',
-        'USER': 'pickling',
+        'NAME': 'sqlite.db',
+        'USER': '',
         'PASSWORD': '',
         'HOST': '',
         'PORT': '',
