36a8bcf42cc262c2adbcb67fee64d20e1cdf5c4c
Alexander Schepanovski <suor.web@gmail.com>
2017-05-22 15:25:13 +0700

    Add settings file to run Django test suite with django-pickling in effect

---
+++ tests/django_settings.py
@@ -0,0 +1,19 @@
+# This is a settings file to run Django tests with django-pickling in effect
+print('Installing django-pickling')
+import django_pickling
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+    },
+    'other': {
+        'ENGINE': 'django.db.backends.sqlite3',
+    }
+}
+
+SECRET_KEY = "django_tests_secret_key"
+
+# Use a fast hasher to speed up tests.
+PASSWORD_HASHERS = [
+    'django.contrib.auth.hashers.MD5PasswordHasher',
+]
